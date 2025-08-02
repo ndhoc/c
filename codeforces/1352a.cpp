0c7a3f3437b46364d789e5180292a0de30a09fe5
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-08-02 22:07:05 +0700

    feat: add 1352a.cpp

---
+++ codeforces/1352a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define pb push_back
+#define el "\n"
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int main() {
+	op();int t;cin>>t;
+	while(t--){
+		string s;cin>>s;
+		int k=s.size();
+		vector<int> a;
+		for(int i=0;i<s.size();++i)
+			if(s[i]!='0')
+				a.pb((s[i]-'0')*pow(10,k-i-1));
+		cout<<a.size()<<el;
+		for(int i=0;i<a.size();++i)
+			cout<<a[i]<<" ";
+		cout<<el;
+	}
+	return 0;
+}
