e6ffbc52415845d5004bd68a1c33f98c8c67edd0
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-08-14 22:44:08 +0700

    Create vl01.cpp

---
+++ luyencode/vl01.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main() {
+	op();
+	int a,b;cin>>a>>b;
+	for(int i=a; i<=b;++i) cout << i <<" ";
+	return 0;
+}
