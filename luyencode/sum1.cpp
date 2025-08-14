d9b6cc029e8a429061a080bf62bbbbd86b340315
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-08-14 22:44:25 +0700

    Create sum1.cpp

---
+++ luyencode/sum1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main() {
+	op();
+	int n;cin>>n; cout << (n*(n+1))/2;
+	return 0;
+}
