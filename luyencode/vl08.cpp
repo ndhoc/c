38ee6082a0237448e7b15e03964e9b8ef9bd2aec
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-08-14 22:46:50 +0700

    Create vl08.cpp

---
+++ luyencode/vl08.cpp
@@ -0,0 +1,45 @@
+/*
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \|     |// '.
+//                 / \|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//            Phật phù hộ, không bao giờ BUG
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define db double
+#define vt vector
+#define pii pair<int,int>
+#define pb push_back
+#define el "\n"
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int main() {
+    op();int a,b,s=0;cin>>a>>b;
+    for(int i=a;i<=b;++i){
+        if(i%2==0)s+=i;
+    }
+    cout<<s;
+    return 0;
+}
