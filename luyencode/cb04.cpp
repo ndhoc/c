cc901b931c41824b546ebb878bc016cdbcd03618
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-08-15 22:01:41 +0700

    Update cb04.cpp

--- luyencode/cb04.cpp
+++ luyencode/cb04.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <cmath>
 #include <iomanip>
 using namespace std;
 
+
 int main(){
-	double a, b; cin >> a >> b;
-
-	double x = a + b;
-	double y = a - b;
-	double z = a * b;
-	
-
-	cout << x << "\n";
-	cout << y << "\n";
-	cout << z << "\n";
-	if(b == 0) cout << "ERROR";
+	double a,b; cin>>a>>b;
+	cout<<a+b<<endl;
+	cout<<a-b<<endl;
+	cout<<a*b<<endl;
+	if(b==0){
+		cout<<"ERROR";
+	}
 	else{
-		double t = (double)(a / b);
-		cout << fixed << setprecision(2) << t;
+		double e = a/b;
+		cout << fixed <<setprecision(2) << e << endl;
 	}
 	return 0;
 }
