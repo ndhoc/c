d4c1eb571e00d5f96b5d3ca770424316d999e8e6
ndhoc <rndhoc@gmail.com>
2025-08-17 22:51:22 +0700

    feat: add 32.c

---
+++ 300baicode/32.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;scanf("%d%d",&a,&b);
+    if(a<b) printf("An nhanh hon");
+    if(a>b) printf("Binh nhanh hon");
+    if(a==b) printf("Bang nhau");
+    return 0;
+}
