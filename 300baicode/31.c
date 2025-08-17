29127a79d322eaacbf63718d1741aa2d2214e77b
ndhoc <rndhoc@gmail.com>
2025-08-17 22:46:31 +0700

    feat: edit 31.c

--- 300baicode/31.c
+++ 300baicode/31.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int a,b;
+    float a,b;
     scanf("%f%f",&a,&b);
     if(a>b){
         printf("An gioi hon");
-        return 0;
     }
     if(a<b){
         printf("Binh gioi hon");
-        return 0;
     }
     if(a==b){
         printf("Bang nhau");
-        return 0;
     }
     return 0;
 }
