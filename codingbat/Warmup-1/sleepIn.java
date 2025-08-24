0f1a4d5831f148eb1d66938eacb692ff837092f2
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-08-24 15:38:39 +0700

    feat: add

---
+++ codingbat/Warmup-1/sleepIn.java
@@ -0,0 +1,10 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+  if(weekday){
+    if(vacation) return true;
+    return false;
+  }
+  else{
+    if(vacation) return true;
+    return true;
+  }
+}
