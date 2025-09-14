41e0abe737ab690fda5e87d42891a1499e9cb3ad
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-09-14 08:45:28 +0700

    Create Hello World! Java program

---
+++ 300 bài Code MeLy/Hello World!.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.print("dap an nhu de bai yeu cau.");
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
