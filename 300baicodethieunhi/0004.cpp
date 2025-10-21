0ab4da1284cf121c27af848598ba1c800f7871e8
ndhoc <rndhoc@gmail.com>
2025-10-21 09:30:42 +0700

    Rename to 300baicodethieunhi add push 5 problem

---
+++ 300baicodethieunhi/0004.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double a; cin>>a; cout<<fixed<<setprecision(2)<<a/3.0;
+    return 0;
+}
--- 300baicodethieunhi/README.md
+++ 300baicodethieunhi/README.md
@@ -1,3 +1,5 @@
 ### HEHEHEHE
 
 [300bàicode.vn](https://xn--300bicode-t1a.vn/)
+
+LINK: https://xn--300bicode-t1a.vn/problem/{namefile}
