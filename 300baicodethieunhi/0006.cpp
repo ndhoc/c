dcb7aa07060c575ac017bc3b1726b07e99f141ad
ndhoc <rndhoc@gmail.com>
2025-10-21 09:33:31 +0700

    feat: add 0006.cpp

---
+++ 300baicodethieunhi/0006.cpp
@@ -0,0 +1,15 @@
+/**
+ *    author: ndhoc
+ *    created: 2025-10-21 09:31:33
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define db double
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int a; cin>>a;
+    cout<<fixed<<setprecision(2)<<(db)sqrt(a);  
+    return 0;
+}
