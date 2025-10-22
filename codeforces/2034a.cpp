cbe26c1ae9b47bae56c1b6fd9712a3a99908a155
ndhoc <rndhoc@gmail.com>
2025-10-22 20:19:06 +0700

    A. King Keykhosrow's Mystery

---
+++ codeforces/2034a.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author: ndhoc
+ *    created: 2025-10-22 20:14:53
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int t; cin>>t;
+    while(t--){
+        int a,b; cin>>a>>b;
+        cout<<a*b/__gcd(a,b)<<"\n";
+    }
+    return 0;
+}
