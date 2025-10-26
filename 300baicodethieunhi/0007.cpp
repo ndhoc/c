7633e7e2518ac86b29550c36b3e03106fd40c792
ndhoc <rndhoc@gmail.com>
2025-10-26 08:15:39 +0700

    feat: add

---
+++ 300baicodethieunhi/0007.cpp
@@ -0,0 +1,14 @@
+/**
+ *    author: ndhoc
+ *    created: 2025-10-26 08:13:16
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    double a; cin>>a; cout<<fixed<<setprecision(5)<<(double)(1/a);
+    return 0;
+}
