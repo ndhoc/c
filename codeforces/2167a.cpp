9f1d28618ac0213506f2035867d1e88d8e1dc4b1
Nguyen Duc Hoc <rndhoc@gmail.com>
2025-10-28 22:55:14 +0700

    Create 2167a.cpp

---
+++ codeforces/2167a.cpp
@@ -0,0 +1,19 @@
+/**
+ *    author: ndhoc
+ *    created: 2025-10-28 21:33:56
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int t; cin>>t; 
+    while(t--){
+        int a,b,c,d; cin>>a>>b>>c>>d;
+    if(a==b && a==c && a==c && a==d) cout<<"YES\n";
+    else cout<<"NO\n";
+    }
+    return 0;
+}
