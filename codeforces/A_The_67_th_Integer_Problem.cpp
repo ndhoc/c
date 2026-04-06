739bd9596804093fbde329337cfd9db3374d7e1f
Nguyen Duc Hoc <rndhoc@gmail.com>
2026-04-06 18:25:01 +0700

    Add solution for Codeforces 67th Integer Problem
    This file implements a solution for the 67th integer problem on Codeforces, reading multiple test cases and outputting the integer for each case.

---
+++ codeforces/A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,18 @@
+/**
+ *    hoc0g
+ *    created: 2026-04-06 18:20:26
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int t; cin >> t; 
+    while(t--){
+        int x; cin >> x; 
+        cout << x << endl;
+    }
+    return 0;
+}
