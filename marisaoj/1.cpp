6852aeac94883deb5445cab2967d524ed74d7ec2
Nguyen Duc Hoc <rndhoc@gmail.com>
2026-05-06 20:31:01 +0700

    Hello Marisa!

---
+++ marisaoj/1.cpp
@@ -0,0 +1,14 @@
+/**
+ *    newbie: ndhoc
+ *    created: 2026-05-06 20:30:42
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cout << "Hello Marisa!";
+    return 0;
+}
