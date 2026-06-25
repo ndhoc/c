fc8b2bbdb9112b9a1761b6ce962593eeded764da
Nguyen Duc Hoc <rndhoc@gmail.com>
2026-06-25 18:17:25 +0700

    D. Tổng đoạn con lớn nhất

---
+++ codeforces/group/NQ Coding Class 2/Chuyen de 07 - Mang cong don (Prefix Sum)/D. Tổng đoạn con lớn nhất.cpp
@@ -0,0 +1,25 @@
+/**
+ *    newbie: ndhoc
+ *    created: 2026-06-25 18:09:27
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n; cin >> n;
+    
+    int64_t cur = 0, ans = LLONG_MIN;
+
+    while(n--) {
+        int64_t x; cin >> x;
+        cur = max(x, cur + x);
+        ans = max(ans, cur);
+
+    }
+
+    cout << ans;
+    return 0;
+}
