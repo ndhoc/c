e458342533807cfb36ef1cd2b44dbac56ef2c725
Nguyen Duc Hoc <rndhoc@gmail.com>
2026-06-26 20:56:25 +0700

    Tổng 3K (THTA Thanh Khê 2022)

---
+++ lqdoj/22tkha2.cpp
@@ -0,0 +1,22 @@
+/**
+ *    newbie: ndhoc
+ *    created: 2026-06-26 20:36:04
+**/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+const int64_t MAXN = 1'000'000'000'005;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    
+    int64_t k; cin >> k;
+
+    int64_t ans = 33+9*k;
+
+    cout << ans;
+
+    return 0;
+}
